7d5ca7c303213412fa09daae2542074eb44a364e
hannikagabrielsson <r.hannika@gmail.com>
2024-03-14 17:58:34 +0200

    n/a

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ForgotPasswordPage } from './components/ForgotPasswordPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
     <AuthProvider>
       <App />
       {/* <BrowserRouter>
@@ -23,5 +23,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </Routes>
       </BrowserRouter> */}
     </AuthProvider>
-  </React.StrictMode>,
 )
